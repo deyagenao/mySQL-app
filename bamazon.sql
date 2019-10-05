2198f596f9cff08f063e3733d700728ba1bcb0fe
Deya Genao <deyagenao@gmail.com>
2019-10-05 12:57:12 -0400

    created initial files

---
+++ bamazon.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+	item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT(10) NOT NULL,
+    PRIMARY KEY (item_id)
+	);
+    
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+	VALUES ("Shirt", "Clothing", 25.75, 50), 
+		("Pants", "Clothing", 40.25, 60),
+        ("Sneakers", "Shoes", 55.00, 80),
+        ("Knee High Boots", "Shoes", 120.89, 30),
+        ("Coffee Maker", "Kitchen", 67.49, 40),
+        ("Pillows Set of 2", "Home", 35.40, 10),
+        ("Vitamins", "Health", 16.89, 20),
+        ("Flat Screen TV", "Electronics", 619.25, 24),
+        ("Wireless Headphones", "Electronics", 99.95, 30),
+        ("Laptop Sleeve", "Electronic Accessories", 20.75, 45);
