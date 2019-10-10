b84922ce0617037b259c2d02ca9785287bc90acc
Deya Genao <deyagenao@gmail.com>
2019-10-09 21:04:18 -0400

    if user makes a purchase, the stock quantity in database will be depleted

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,24 +24,15 @@ connection.connect(function(err) {
     if (err) throw err;
 
     // function to begin the bamazon program
-    startBamazon();
-    // requestProductPurchase();
-});
-
-// function to start bamazon and show the available products 
-function startBamazon() {
-
-    console.log("\n\nWelcome to Bamazon! These are the items we currently have available for purchase: \n\n");
-
     showProducts();
-
-}
-
+    
+});
 
 
 
 // function for showing available products 
 function showProducts(){
+    console.log("\n\nWelcome to Bamazon! These are the items we currently have available for purchase: \n\n");
 
     connection.query("SELECT * FROM products", function(err, data){
         
@@ -49,11 +40,22 @@ function showProducts(){
 
         // for loop to print all of the item data to the console 
         for (var i=0; i < data.length; i++){
-            console.log(data[i].item_id, "||", data[i].product_name, "||", data[i].department_name, "|| $" + data[i].price, "|| Quantity in Stock:", data[i].stock_quantity+ "\n");
+            console.log(data[i].item_id, data[i].product_name, "||", data[i].department_name, "|| $" + data[i].price.toFixed(2), "|| Quantity in Stock:", data[i].stock_quantity+ "\n");
         }
 
-        makePurchase();
-
+        // prompt the user to see if they would like to run the program or exit 
+        inquirer.prompt({
+            name: "userAction",
+            type: "rawlist",
+            message: "What would you like to do now?",
+            choices: ["Make a purchase.", "Exit Bamazon."]
+        }).then(function(answer){
+            if(answer.userAction === "Make a purchase."){
+                makePurchase();
+            }else{
+                process.exit();
+            }
+        })
     });
 
 }
@@ -87,47 +89,65 @@ function makePurchase(){
             }
         }
     ]).then(function(answers){
-        // check if the product ID entered actually matches one that is available 
+
+            // begin query to the bamazon database 
             connection.query("SELECT item_id, stock_quantity, price FROM products WHERE ?", 
             {
                 item_id: answers.productId
             },
             function(err, data){
                 if (err) throw err;
+
+                // check if the product ID entered actually matches one that is available 
                 var productExists = false;
+
+                // loop through the result
                 for (var i = 0; i < data.length; i++){
+
+                    // if the product exists
                     if (data[i].item_id == answers.productId){
+                        // change the value of productExists to true 
                         productExists = true;
-                        console.log("please work")
-                        if (data[i].stock_quantity > parseInt(answers.productQuantity)){
-                            console.log("Great! \n")
-                            console.log("Your total will be: \n $" + calculatePrice(parseInt(answers.productQuantity), data[i].price) + "\nThank you for your purchase.");
+
+                        // check if there is sufficient quantity to make the purchase 
+                        if (data[i].stock_quantity >= parseInt(answers.productQuantity)){
+                            
+                            // if true, return the price to the user 
+                            console.log("Great! \nYour total will be: \n $" + calculatePrice(parseInt(answers.productQuantity), data[i].price) + "\nThank you for your purchase.");
+
+                            // update product quantity in the database to reflect the purchase 
+                            connection.query("UPDATE products SET ? WHERE ?", 
+                            [
+                                {stock_quantity: data[i].stock_quantity - answers.productQuantity},
+
+                                {item_id: answers.productId}
+                            ], 
+                            function (err) {
+                                if (err) throw err;
+                            }
+                            )
+
                         }else{
+                            // return message to user if there is an insufficient quantity of the product available 
                             console.log("Sorry, insufficient quantity available.")
                         }
                     }
                 }
+
+                // if the product does not exist, tell the user 
                 if(productExists === false){
-                    console.log("Please enter a valid product ID number.")
+                    console.log("Oops, please enter a valid product ID number.")
                 }
                 
-                
-
-                // console.log(data);
-                // console.log(data.item_id);
-                // if (thisProductId === answers.productId){
-                //     if(data.stock_quantity < answers.productQuantity){
-                //         console.log("Sorry, insufficient quantity!")
-                //     }
-                    
-                // }else {
-                //     console.log("Please enter a valid product ID number.")
-                // }
+                // run the showProducts function to begin the program again
                 showProducts();
             })
     })
 }
 
+
+// function for returning the price of purchases made 
+
 function calculatePrice(quant, price){
-    return quant*price;
+    return (quant*price).toFixed(2);
 }
